fb1a2ad8c10f5dbbe5b3b1e4730a1d7e570d0a4d
Ricardo Poleo <ricardopoleo@gmail.com>
2024-02-05 14:08:20 -0500

    Initial draw and motions
    Added initial draw of the rectangle.
    Added Left and Right movements.
    Added limitation to avoid the rectangle going off screen.

---
+++ DrawBlock.py
@@ -0,0 +1,48 @@
+import pygame
+
+SCREEN_WIDTH = 400
+
+pygame.init()
+
+surface = pygame.display.set_mode((SCREEN_WIDTH, 300))
+pygame.display.set_caption('Down Well Clone')
+color = (255, 0, 0)
+
+# Initial position of the rectangle
+rect_x = 30
+rect_y = 30
+
+# Rectangle dimensions
+rect_width = 30
+rect_height = 30
+
+# Movement speed
+speed = 0.1
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+
+    # Update the position based on key presses
+    if keys[pygame.K_LEFT]:
+        rect_x -= speed
+    if keys[pygame.K_RIGHT]:
+        rect_x += speed
+
+    # Boundary checks to keep the rectangle within the screen
+    if rect_x < 0:
+        rect_x = 0
+    if rect_x + rect_width > SCREEN_WIDTH:  # Assuming screen width is 400
+        rect_x = SCREEN_WIDTH - rect_width
+
+    # Clear the screen
+    surface.fill((0, 0, 0))
+
+    # Draw the rectangle at the updated position
+    pygame.draw.rect(surface, color, pygame.Rect(rect_x, rect_y, rect_width, rect_height))
+    pygame.display.flip()
--- readme.md
+++ readme.md
@@ -8,12 +8,28 @@ It is a clone of the game DownWell: tt is a game where you fall down a well and 
 
 The game is done initially with PyGame. 
 
+## Getting Started
+
+To start developing the game, you need to have Python 3.7 or higher installed in your computer. Also you need to have the PyGame library installed.
+
+To install PyGame, you can use the following command:
+
+```bash
+pip install pygame
+```
+
+You can also use the requirements.txt file to install all the dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
 ## Initial steps:
 
-- [ ] Create a window
-- [ ] Display a box in the screen
-- [ ] Make the box move (Left, Right)
-- [ ] Avoid the box to go out of the screen
+- [X] Create a window
+- [X] Display a box in the screen
+- [X] Make the box move (Left, Right)
+- [X] Avoid the box to go out of the screen
 - [ ] Make the box fall
 - [ ] Add obstacles
 - [ ] Add collision with obstacles
